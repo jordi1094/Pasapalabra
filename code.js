d55f7f46af3382d57d855ac8e290b19993372bcb
jordi1094 <63929965+jordi1094@users.noreply.github.com>
2024-03-14 17:52:47 +0100

    Finalizado

--- code.js
+++ code.js
@@ -4,251 +4,235 @@ let preguntas = [
         "pregunta": "CON LA A: Mecanismo para esparcir un líquido a presión, como el agua para el riego",
         "respuesta": "ASPERSOR",
         "solucion": "undefined",
-        "numeroLetra":"1" 
+        "numeroLetra": 1 
     },
     {
         "letra": "B",
         "pregunta": "CON LA B: Apertura delantera de un pantalón",
         "respuesta": "BRAGUETA",
         "solucion": "undefined",
-        "numeroLetra":"2"
+        "numeroLetra": 2
     },
     {
         "letra": "C",
         "pregunta":"CON LA C: Galería que rodea el patio principal de una iglesia o convento",
         "respuesta": "CLAUSTRO",
         "solucion": "undefined",
-        "numeroLetra":"3"
+        "numeroLetra": 3
     },
     {
         "letra": "D",
         "pregunta": "CON LA D: Documento que expide un centro educativo o una corporación y que acredita un título o un grado académico",
         "respuesta": "DIPLOMA",
         "solucion": "undefined",
-        "numeroLetra":"4"
+        "numeroLetra": 4
     },
     {
         "letra": "E",
         "pregunta": "CON LA E: Nombre del rey visigodo que gobernó entre los años 687 y 702, y limitó la actividad económica de los judíos",
         "respuesta": "ÉGICA",
         "solucion": "undefined",
-        "numeroLetra":"5"
+        "numeroLetra": 5
     },
     {
         "letra": "F",
         "pregunta": "CON LA F: Brillo muy intenso",
         "respuesta": "FULGOR",
         "solucion": "undefined",
-        "numeroLetra":"6"
+        "numeroLetra": 6
     },
     {
         "letra": "G",
         "pregunta":"CON LA G: País asiático con capital en Tiflis", 
         "respuesta": "GEORGIA",
         "solucion": "undefined",
-        "numeroLetra":"7"
+        "numeroLetra": 7
     },
     {
         "letra": "H",
         "pregunta":"CON LA H: Masa de harina con manteca, muy sobada y que, al cocerse en el horno, forma muchas hojas delgadas superpuestas",
         "respuesta": "HOJALDRE",
         "solucion": "undefined",
-        "numeroLetra":"8"
+        "numeroLetra": 8
     },
     {
         "letra": "I",
         "pregunta": "CON LA I: Esperanza cuyo cumplimiento parece especialmente atractivo",
         "respuesta": "ILUSIÓN",
         "solucion": "undefined",
-        "numeroLetra":"9"
+        "numeroLetra": 9
     },
     {
         "letra": "J",
         "pregunta": "CON LA J: Soldado de infantería de la Guardia Imperial turca, reclutado a menudo entre hijos de cristianos",
         "respuesta": "JENÍZARO",
         "solucion": "undefined",
-        "numeroLetra":"10"
+        "numeroLetra": 10
     },
     {
         "letra": "K",
         "pregunta": "CON LA K: Ropa que utilizan los japoneses",
         "respuesta" : "KIMONO",
         "solucion": "undefined",
-        "numeroLetra":"11"
+        "numeroLetra": 11
     },
     {
         "letra": "L",
         "pregunta": "CON LA L: Daño o detrimento corporal causado por una herida, un golpe o una enfermedad",
         "respuesta": "LESIÓN",
         "solucion": "undefined",
-        "numeroLetra":"12"
+        "numeroLetra": 12
     },
     {
         "letra": "M",
         "pregunta": "CON LA M: Apellido del matemático que, a principios del siglo 20, introdujo el modelo de espacio-tiempo en cuatro dimensiones",
         "respuesta": "MINKOWSKI",
         "solucion": "undefined",
-        "numeroLetra":"13"
+        "numeroLetra": 13
     },
     {
         "letra": "N",
         "pregunta": "CON LA N: Insignificante, sin importancia",
         "respuesta": "NIMIO",
         "solucion": "undefined",
-        "numeroLetra":"14"
+        "numeroLetra": 14
     },
     {
         "letra": "Ñ",
         "pregunta": "CONTIENE LA Ñ: En fútbol, acción en la que se sortea a un contrario pasándole el balón entre las piernas",
         "respuesta": "CAÑO",
         "solucion": "undefined",
-        "numeroLetra":"15"
+        "numeroLetra": 15
     },
     {
         "letra": "O",
         "pregunta": "CON LA O: Cerro aislado que domina un llano",
         "respuesta": "OTERO",
         "solucion": "undefined",
-        "numeroLetra":"16"
+        "numeroLetra": 16
     },
     {
         "letra": "P",
         "pregunta": "CON LA P: Juego que consiste en tirar unas bolas y otra más pequeña, y ver quién se aproxima más a ésta con las grandes",
         "respuesta": "PETANCA",
         "solucion": "undefined",
-        "numeroLetra":"17"
+        "numeroLetra": 17
     },
     {
         "letra": "Q",
         "pregunta": "CONTIENE LA Q: Se dice del triángulo que tiene todos sus lados iguales",
         "respuesta": "EQUILATERO",
         "solucion": "undefined",
-        "numeroLetra":"18"
+        "numeroLetra": 18
     },
     {
         "letra": "R",
         "pregunta": "CON LA R: Satisfacción a una pregunta, duda o dificultad",
         "respuesta": "RESPUESTA",
         "solucion": "undefined",
-        "numeroLetra":"19"
+        "numeroLetra": 19
     },
     {
         "letra": "S",
         "pregunta": "CON LA S: Antiguo instrumento musical de metal, consistente en un aro o herradura atravesado por varillas, que sonaba agitándolo",
         "respuesta": "SISTRO",
         "solucion": "undefined",
-        "numeroLetra":"20"
+        "numeroLetra": 20
     },
     {
         "letra": "T",
         "pregunta": "CON LA T: Cada uno de los pasos o gestiones que se establecen de manera oficial para la conclusión o resolución de un asunto",
         "respuesta": "TRÁMITE",
         "solucion": "undefined",
-        "numeroLetra":"21"
+        "numeroLetra": 21
     },
     {
         "letra": "U",
         "pregunta": "CON LA U: Lugar en el que está situado algo",
         "respuesta": "UBICACIÓN",
         "solucion": "undefined",
-        "numeroLetra":"22"
+        "numeroLetra": 22
     },
     {
         "letra": "V",
         "pregunta": "CON LA V: Casa de recreo aislada en el campo",
         "respuesta": "VILLA",
         "solucion": "undefined",
-        "numeroLetra":"23"
+        "numeroLetra": 23
     },
     {
         "letra": "W",
         "pregunta": "CONTIENE LA W: fruto comestible, de piel vellosa y pulpa verde",
         "respuesta": "KIWI",
         "solucion": "undefined",
-        "numeroLetra":"24"
+        "numeroLetra": 24 
     },
     {
         "letra": "X",
         "pregunta": "CONTIENE LA X: Enunciado o conjunto coherente de enunciados orales o escritos",
         "respuesta": "TEXTO",
         "solucion": "undefined",
-        "numeroLetra":"25"
+        "numeroLetra": 25
     },
     {
         "letra": "Y",
         "pregunta": "CONTIENE LA Y: Máquina o conjunto de máquinas que sirven para cambiar los decorados en el escenario de un teatro",
         "respuesta": "TRAMOYA",
         "solucion": "undefined",
-        "numeroLetra":"26"
+        "numeroLetra": 26
     },
     {
         "letra": "Z",
         "pregunta": "CON LA Z: Escarabajo que ataca los trigales, especialmente cuando los granos son tiernos",
         "respuesta": "ZABRO",
         "solucion": "undefined",
-        "numeroLetra":"27"
+        "numeroLetra": 27
     }
 ];
-let letras= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-let preguntaActual = "";
-let respuestaActual = "";
-let contador = 1;
 let displayPregunta = document.getElementsByClassName("pregunta")[0];
 let displayRespuesta = document.getElementById("respuesta");
 let botonResponder = document.getElementById("enviar");
-let j = 0
+let contador = 0;
+let preguntasCorrectas = [];
+let preguntasIncorrectas = [];
 
-preguntaActual = preguntas[contador-1].pregunta;
+preguntaActual = preguntas[contador].pregunta;
 displayPregunta.innerHTML = preguntaActual
     displayRespuesta.value = ""
 
 botonResponder.addEventListener("click",() =>{
-    if(contador < 26){
-    Responder()}
-    if(contador >=26){
-        contador++
-        console.log(contador, "2")
-        for(j=0; j<=26;j++){
-            if(preguntas[j].solucion === "undefined"){
-                PreguntarPasapalabras(j)
-            }}}
+
+    Responder()
 })
 
 function Responder(){
-            console.log(contador, "1")
-        respuestaActual =displayRespuesta.value
-        if(respuestaActual.toUpperCase() === preguntas[contador-1].respuesta){
-            let circuloLetra = document.getElementById(letras[contador-1]);
-            circuloLetra.style.backgroundColor = "green"
-            preguntas[contador-1].solucion = "correcta"
-        }if(respuestaActual.toUpperCase() === "PASAPALABRA"){
-            let circuloLetra = document.getElementById(letras[contador-1]);
-            circuloLetra.style.backgroundColor = "orange"
-        }if(respuestaActual.toUpperCase() !== preguntas[contador-1].respuesta && respuestaActual.toUpperCase() !== "PASAPALABRA"){
-            let circuloLetra = document.getElementById(letras[contador-1]);
-            circuloLetra.style.backgroundColor = "red"
-            preguntas[contador-1].respuesta = "incorrecta"
-        }
-    contador++    
-    preguntaActual = preguntas[contador-1].pregunta;
-    displayPregunta.innerHTML = preguntaActual
-    displayRespuesta.value = ""
-}
-
-function PreguntarPasapalabras(){    
-    if(respuestaActual.toUpperCase() === preguntas[j].respuesta){
-        let circuloLetra = document.getElementById(letras[j]);
+    let respuestaActual =displayRespuesta.value.toUpperCase()
+    let circuloLetra = document.getElementById(preguntas[contador].letra)
+    if(respuestaActual === preguntas[contador].respuesta){
         circuloLetra.style.backgroundColor = "green"
-        preguntas[j].solucion = "correcta"
-    }if(respuestaActual.toUpperCase() === "PASAPALABRA"){
-        circuloLetra.style.backgroundColor = "orange"
-    }if(respuestaActual.toUpperCase() !== preguntas[j].respuesta && respuestaActual.toUpperCase() !== "PASAPALABRA"){
-        let circuloLetra = document.getElementById(letras[j]);
+    }if(respuestaActual !== preguntas[contador].respuesta && respuestaActual !== "PASAPALABRA"){
         circuloLetra.style.backgroundColor = "red"
-        preguntas[j].respuesta = "incorrecta"
     }
-    preguntaActual = preguntas[j].pregunta;
-    displayPregunta.innerHTML = preguntaActual
-    displayRespuesta.value = "" 
-};
+    if(respuestaActual === 'PASAPALABRA'){
+        circuloLetra.style.backgroundColor = "orange"
+        contador++
+        if(contador > preguntas.length){
+            contador = 0
+        }
+    }if(respuestaActual !== 'PASAPALABRA'){
+        preguntas.splice(contador,1)
+    }if(contador > preguntas.length-1){
+        contador = 0
+    }
+    console.log(preguntas.length)
 
+    if(preguntas.length === 0){
+        preguntaActual = "Fin del juego. ¡Muchas gracias por jugar!"
+        botonResponder.style.opacity = "0%"
+        displayRespuesta.style.opacity = "0%"
+        displayPregunta.style.height = "25vh"
+        displayPregunta.innerHTML = preguntaActual
+    }else{    preguntaActual = preguntas[contador].pregunta;
+        displayPregunta.innerHTML = preguntaActual
+            displayRespuesta.value = ""}
+};
